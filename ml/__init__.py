05b397669ff14a0292a42b68d07a4ff14781c92a
Shruti Rai <shruti.rai2901@gmail.com>
2026-09-16 03:26:30 +0530

    fix(ml): make package imports resilient to avoid numpy dependency crash in serverless

--- ml/__init__.py
+++ ml/__init__.py
@@ -1,11 +1,32 @@
-from .feature_pipeline import DeveloperFeaturePipeline
-from .archetype_clusterer import ArchetypeClusterer
-from .career_recommender import CareerRecommender
-from .skill_simulator import SkillPathSimulator
+try:
+    from .feature_pipeline import DeveloperFeaturePipeline
+except ImportError:
+    DeveloperFeaturePipeline = None
+
+try:
+    from .archetype_clusterer import ArchetypeClusterer
+except ImportError:
+    ArchetypeClusterer = None
+
+try:
+    from .career_recommender import CareerRecommender
+except ImportError:
+    CareerRecommender = None
+
+try:
+    from .skill_simulator import SkillPathSimulator
+except ImportError:
+    SkillPathSimulator = None
+
+try:
+    from .chatbot_agent import TrainedCodeDNAAgent
+except ImportError:
+    TrainedCodeDNAAgent = None
 
 __all__ = [
     "DeveloperFeaturePipeline",
     "ArchetypeClusterer",
     "CareerRecommender",
-    "SkillPathSimulator"
+    "SkillPathSimulator",
+    "TrainedCodeDNAAgent"
 ]
